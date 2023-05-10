15ec7c51d693dbd59d69ac5ae1ef5fb96acacc58
fuckwd <fuckwd@ss14.su>
2023-05-11 01:44:25 +0600

    [Fluff] murka extended

--- Content.Server/White/Other/Lazy/EarsSpawnComponent.cs
+++ Content.Server/White/Other/Lazy/EarsSpawnComponent.cs
@@ -6,11 +6,22 @@ namespace Content.Server.White.Other.Lazy;
 [RegisterComponent]
 public sealed class EarsSpawnComponent : Component
 {
-    [DataField("summonAction")] public InstantAction SummonAction = new()
+    public InstantAction SummonAction = new()
     {
         Icon = new SpriteSpecifier.Texture(new ResPath("Clothing/Head/Hats/witch.rsi/icon.png")),
         DisplayName = "summon cat ears",
         Description = "meow!",
         Event = new SummonActionEarsEvent()
     };
+
+    public InstantAction SummonActionCat = new()
+    {
+        Icon = new SpriteSpecifier.Texture(new ResPath("Clothing/Head/Hats/witch.rsi/icon.png")),
+        DisplayName = "summon cat",
+        Description = "meow!",
+        Event = new SummonActionCatEvent()
+    };
+
+    public int CatEarsUses = 30;
+    public int СatSpawnUses = 20;
 }
--- Content.Server/White/Other/Lazy/EarsSpawnSystem.cs
+++ Content.Server/White/Other/Lazy/EarsSpawnSystem.cs
@@ -1,5 +1,6 @@
 using Content.Shared.ActionBlocker;
 using Content.Shared.Actions;
+using Content.Shared.Examine;
 using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Popups;
 using Content.Shared.Verbs;
@@ -20,10 +21,19 @@ public sealed class EarsSpawnSystem : EntitySystem
         SubscribeLocalEvent<EarsSpawnComponent, GetVerbsEvent<AlternativeVerb>>(AddSummonVerb);
         SubscribeLocalEvent<EarsSpawnComponent, GetItemActionsEvent>(GetSummonAction);
         SubscribeLocalEvent<EarsSpawnComponent, SummonActionEarsEvent>(OnSummon);
+        SubscribeLocalEvent<EarsSpawnComponent, SummonActionCatEvent>(OnSummonCat);
+        SubscribeLocalEvent<EarsSpawnComponent, ExaminedEvent>(OnExamined);
     }
 
     private const string Ears = "ClothingHeadHatCatEars";
+    private const string Cat = "MobCatMurka";
     private const string UserNeededKey = "merkkaa";
+
+    private void OnExamined(EntityUid u, EarsSpawnComponent comp, ExaminedEvent ev)
+    {
+        ev.PushMarkup($"Зарядов для ушей: {comp.CatEarsUses}\n" +
+                      $"Зарядов для создания кота: {comp.СatSpawnUses}");
+    }
 
     private void AddSummonVerb(EntityUid uid, EarsSpawnComponent component, GetVerbsEvent<AlternativeVerb> args)
     {
@@ -40,12 +50,28 @@ public sealed class EarsSpawnSystem : EntitySystem
             Priority = 2
         };
 
+        AlternativeVerb verbCat = new()
+        {
+            Act = () =>
+            {
+                AttemptSummonCat(component, args.User);
+            },
+            Text = Loc.GetString("summon cat"),
+            Priority = 3
+        };
+
         args.Verbs.Add(verb);
+        args.Verbs.Add(verbCat);
     }
 
     private void OnSummon(EntityUid uid, EarsSpawnComponent component, SummonActionEarsEvent args)
     {
         AttemptSummon(component, args.Performer);
+    }
+
+    private void OnSummonCat(EntityUid uid, EarsSpawnComponent component, SummonActionCatEvent args)
+    {
+        AttemptSummonCat(component, args.Performer);
     }
 
     private void AttemptSummon(EarsSpawnComponent component, EntityUid user)
@@ -63,10 +89,40 @@ public sealed class EarsSpawnSystem : EntitySystem
             }
         }
 
-        SpawnEars(user);
+        if (component.CatEarsUses == 0)
+        {
+            _popupSystem.PopupEntity("Больше нет зарядов!", user, PopupType.Medium);
+            return;
+        }
+
+        SpawnEars(user, component);
     }
 
-    private void SpawnEars(EntityUid player)
+    private void AttemptSummonCat(EarsSpawnComponent component, EntityUid user)
+    {
+        if (!_blocker.CanInteract(user, component.Owner))
+            return;
+
+        if (TryComp<ActorComponent>(user, out var actorComponent))
+        {
+            var userKey = actorComponent.PlayerSession.Name;
+            if (userKey != UserNeededKey)
+            {
+                _popupSystem.PopupEntity("Вы не являетесь потомком кошко-богини.", user, PopupType.Medium);
+                return;
+            }
+        }
+
+        if (component.СatSpawnUses == 0)
+        {
+            _popupSystem.PopupEntity("Больше нет зарядов!", user, PopupType.Medium);
+            return;
+        }
+
+        SpawnCat(user, component);
+    }
+
+    private void SpawnEars(EntityUid player, EarsSpawnComponent comp)
     {
         var transform = CompOrNull<TransformComponent>(player)?.Coordinates;
 
@@ -75,14 +131,31 @@ public sealed class EarsSpawnSystem : EntitySystem
 
         var ears = _entityManager.SpawnEntity(Ears, transform.Value);
         _handsSystem.PickupOrDrop(player, ears);
+        comp.CatEarsUses--;
+    }
+
+    private void SpawnCat(EntityUid player, EarsSpawnComponent comp)
+    {
+        var transform = CompOrNull<TransformComponent>(player)?.Coordinates;
+
+        if (transform == null)
+            return;
+
+        _entityManager.SpawnEntity(Cat, transform.Value);
+        comp.СatSpawnUses--;
     }
 
     private static void GetSummonAction(EntityUid uid, EarsSpawnComponent component, GetItemActionsEvent args)
     {
         args.Actions.Add(component.SummonAction);
+        args.Actions.Add(component.SummonActionCat);
     }
 }
 
 public sealed class SummonActionEarsEvent : InstantActionEvent
+{
+}
+
+public sealed class SummonActionCatEvent : InstantActionEvent
 {
 }
