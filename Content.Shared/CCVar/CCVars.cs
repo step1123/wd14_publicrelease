546096f14b3527532d1b960c7d05836a3c66af6f
fuckwd <fuckwd@ss14.su>
2023-08-17 10:45:09 +0300

    Less objectives (#307)

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -374,7 +374,7 @@ namespace Content.Shared.CCVar
             CVarDef.Create("traitor.starting_balance", 20);
 
         public static readonly CVarDef<int> TraitorMaxDifficulty =
-            CVarDef.Create("traitor.max_difficulty", 8);
+            CVarDef.Create("traitor.max_difficulty", 7);
 
         public static readonly CVarDef<int> TraitorMinObjectives =
             CVarDef.Create("traitor.min_objectives", 3);
