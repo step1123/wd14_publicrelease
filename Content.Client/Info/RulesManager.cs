fbb903b297af922d7ee5b34b7923e674a5cf6b42
fuckwd <fuckwd@ss14.su>
2023-08-19 14:04:30 +0300

    Rules (#319)
    * More rule buttons
    
    * Fix windpw

--- Content.Client/Info/RulesManager.cs
+++ Content.Client/Info/RulesManager.cs
@@ -1,5 +1,6 @@
 using Content.Client.Lobby;
 using Content.Client.Gameplay;
+using Content.Client.White.Rules;
 using Content.Shared.CCVar;
 using Content.Shared.Info;
 using Robust.Client.Console;
@@ -100,6 +101,6 @@ public sealed class RulesManager : SharedRulesManager
     {
         rulesSection = new InfoSection("", "", false);
         UpdateRules();
-        return rulesSection;
+        return new RulesWikiSection(); // WD EDIT
     }
 }
