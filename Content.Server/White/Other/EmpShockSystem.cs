5e75ca2a46288c8651cbfffe5d0bf2ba8e53fdc0
fuckwd <fuckwd@ss14.su>
2023-09-23 01:28:30 +0900

    Emp shock (#427)
    * Emp hardsuit shock effect
    
    * Implant charge chage
    
    * Ignore insulated

---
+++ Content.Server/White/Other/EmpShockSystem.cs
@@ -0,0 +1,29 @@
+using Content.Server.Electrocution;
+using Content.Server.Emp;
+using Content.Shared.Inventory;
+using Content.Shared.StatusEffect;
+using Content.Shared.Tag;
+
+namespace Content.Server.White.Other;
+
+public sealed class EmpShockSystem : EntitySystem
+{
+    [Dependency] private readonly InventorySystem _inventorySystem = default!;
+    [Dependency] private readonly TagSystem _tag = default!;
+    [Dependency] private readonly ElectrocutionSystem _electrocution = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<StatusEffectsComponent, EmpPulseEvent>(OnEmpPulse);
+    }
+
+    private void OnEmpPulse(EntityUid uid, StatusEffectsComponent component, ref EmpPulseEvent args)
+    {
+        if (!_inventorySystem.TryGetSlotEntity(uid, "outerClothing", out var suit) ||
+            !_tag.HasTag(suit.Value, "Hardsuit") || _tag.HasTag(suit.Value, "EmpResistance"))
+            return;
+
+        if (_electrocution.TryDoElectrocution(uid, null, 15, TimeSpan.FromSeconds(6), false, 1F, component, true))
+            args.Affected = true;
+    }
+}
