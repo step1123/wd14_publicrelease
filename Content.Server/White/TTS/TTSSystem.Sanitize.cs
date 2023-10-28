f108b86ebb028b1ccf588d7950c18ff031b39893
fuckwd <fuckwd@ss14.su>
2023-10-28 15:49:45 +0300

    fix tts sanitize (#517)

--- Content.Server/White/TTS/TTSSystem.Sanitize.cs
+++ Content.Server/White/TTS/TTSSystem.Sanitize.cs
@@ -18,7 +18,7 @@ public sealed partial class TTSSystem
     {
         text = text.Trim();
         text = Regex.Replace(text, @"Ё", "Е");
-        text = Regex.Replace(text, @"[^a-zA-Zа-яА-ЯёЁ0-9,\-,+, ,?,!]", "");
+        text = Regex.Replace(text, @"[^a-zA-Zа-яА-ЯёЁ0-9,\-,+, ,?,!,.]", "");
         text = Regex.Replace(text, @"[a-zA-Z]", ReplaceLat2Cyr, RegexOptions.Multiline | RegexOptions.IgnoreCase);
         text = Regex.Replace(text, @"(?<![a-zA-Zа-яёА-ЯЁ])[a-zA-Zа-яёА-ЯЁ]+?(?![a-zA-Zа-яёА-ЯЁ])", ReplaceMatchedWord, RegexOptions.Multiline | RegexOptions.IgnoreCase);
         text = Regex.Replace(text, @"(?<=[1-90])(\.|,)(?=[1-90])", " целых ");
